29522ef676d43ab58b60c8f9c37a8b4b8329b213
DanishAnsari5522 <ddanish1928@gmail.com>
2022-12-16 14:05:40 +0530

    Edit Notification

--- src/screen/Home/Notification.js
+++ src/screen/Home/Notification.js
@@ -1,8 +1,9 @@
 import React from "react";
-import { View, Text, StyleSheet ,ScrollView} from 'react-native'
+import { View, Text, StyleSheet, ScrollView ,Dimensions} from 'react-native'
 import Container from "../../component/Container";
 import Theme from "../../component/Theme";
 import NotificationScreenHeader from "../../Header/NotificationScreenHeader";
+import Icon from '@expo/vector-icons/MaterialIcons'
 
 
 function Notification() {
@@ -11,7 +12,43 @@ function Notification() {
             <NotificationScreenHeader />
             <Container>
                 <ScrollView>
-                    <Text style={styles.t}>Notification Screen</Text>
+                        <View style={styles.card}>
+                            <Text style={styles.date}>Today</Text>
+                            <View style={styles.carddetail}>
+                                <View style={styles.offericon}>
+                                    <Icon name="credit-card" color='gray' size={30} />
+                                </View>
+                                <View>
+                                    <Text style={styles.offerdetail}>Danish Ansari</Text>
+                                    <Text style={styles.offerdetail1}>Added You</Text>
+                                </View>
+                            </View>
+                        </View>
+                        <View style={styles.card}>
+                            <Text style={styles.date}>Yestarday</Text>
+                            <View style={styles.carddetail}>
+                                <View style={styles.offericon}>
+                                    <Icon name="lock" color='gray' size={30} />
+                                </View>
+                                <View>
+                                    <Text style={styles.offerdetail}>Ashutosh Tiwari</Text>
+                                    <Text style={styles.offerdetail1}>Reedeem Your Coupan</Text>
+                                </View>
+                            </View>
+                        </View>
+                        <View style={styles.card}>
+                            <Text style={styles.date}>December 10,2024</Text>
+                            <View style={styles.carddetail}>
+                                <View style={styles.offericon}>
+                                    <Icon name='perm-media' color='gray' size={30} />
+                                </View>
+                                <View>
+                                    <Text style={styles.offerdetail}>Deepak Kumar</Text>
+                                    <Text style={styles.offerdetail1}>Added You</Text>
+                                </View>
+                            </View> 
+                        </View>
+                   
                 </ScrollView>
             </Container>
 
@@ -21,10 +58,52 @@ function Notification() {
 
 }
 
+const { width } = Dimensions.get("window")
+
 const styles = StyleSheet.create({
-    t: {
-        color: Theme.colors.textColor
+    container: {
+        width,
+        alignItems: "center",
+        justifyContent: "space-between",
+    },
+    card: {
+        width: '100%'
+    },
+    carddetail: {
+        borderRadius: 10,
+        borderWidth: 1,
+        borderColor: '#ddddda',
+        padding: 15,
+        flex: 1,
+        flexDirection: 'row',
+        alignItems: 'center',
+        marginBottom: 15
+    },
+    date: {
+        fontSize: 16,
+        fontWeight: '500',
+        marginBottom: 10,
+        color:'white'
+    },
+    offerdetail: {
+        fontSize: 18,
+        fontWeight: '500',
+        color:'white'
+    },
+    offerdetail1: {
+        fontSize: 14,
+        color: 'gray'
+    },
+    offericon: {
+        width: 50,
+        height: 50,
+        backgroundColor: '#ddddda',
+        borderRadius: 40,
+        marginRight: 10,
+        alignItems: 'center',
+        justifyContent: 'center'
     }
+
 })
 
 export default Notification;
